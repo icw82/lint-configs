b1a4fc4f145449769c9759fad12a651fe0107d12
icw82 <icw82@yandex.ru>
2023-12-13 19:23:46 +0500

    update

--- eslint/eslint.config.mjs
+++ eslint/eslint.config.mjs
@@ -1,18 +1,18 @@
 import js from '@eslint/js';
 
 import { base } from './base.config.mjs'
-import { typescript } from './typescript.config.mjs'
+import { getEslintTypescriptConfig } from './typescript.config.mjs'
 import { es5 } from './es5.config.mjs'
 
 
-const all = [
+const basic = [
     js.configs.recommended,
     base,
-    typescript,
 ];
 
 
 export {
-    all,
+    basic as base ,
+    getEslintTypescriptConfig,
     es5,
 };
